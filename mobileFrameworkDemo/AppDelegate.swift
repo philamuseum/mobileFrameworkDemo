0c70dd1ccf5aeed1af8123128c02b732b97cbb10
Peter Alt <peter.alt@philamuseum.org>
2017-07-05 17:38:16 -0400

    integrating remote notifications

--- mobileFrameworkDemo/AppDelegate.swift
+++ mobileFrameworkDemo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import mobileFramework
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -39,6 +40,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        BackendService.shared.deviceTokenData = deviceToken
+        do {
+            print("AppDelegate: Registering device to backend")
+            try BackendService.shared.registerDevice()
+        } catch {
+            print("AppDelegate: Error registering device \(error.localizedDescription)")
+        }
     }
 
 
